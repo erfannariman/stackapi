267a48e6044ec45a5511e100836253317c8a3c7b
Erfan Nariman | Veneficus <erfan.nariman@briccanalytics.nl>
2020-08-11 20:16:21 +0200

    BUG: run main for all tables

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from src.api import get_data
 # from src.parse_settings import get_settings
 from src.db import auth_azure
 
-# from src.functions import create_dataframe
 from src.functions import MakeDataFrame
 from src.log import set_logging
 
@@ -14,27 +13,33 @@ def pull_data():
     logging.info("pulling data from Stack API...")
 
     json = get_data()
-    mkdf = MakeDataFrame(json)
-    df = mkdf.create_dataframe()
-    logging.info(f"imported {len(df)} records!")
+    make_df = MakeDataFrame(json)
+    dfs = make_df.create_dataframes()
+    for name, df in dfs.items():
+        logging.info(f"imported {len(df)} {name} records!")
 
-    return df
+    return dfs
 
 
-def upload_data(df):
-    logging.info(f"uploading {len(df)} records to Azure...")
-    engn = auth_azure()
+def upload_data(df, name, db_engine):
+    logging.info(f"uploading {name} to Azure with {len(df)} records...")
     df.to_sql(
-        name="pandas", con=engn, if_exists="replace", schema="method_usage", index=False
+        name=f"pandas_{name}",
+        con=db_engine,
+        if_exists="replace",
+        schema="method_usage",
+        index=False
     )
+
+
+def run():
+    dfs = pull_data()
+    db_engine = auth_azure()
+    for name, df in dfs.items():
+        upload_data(df, name, db_engine)
     logging.info("finished uploading!")
-
-
-def mainscript():
-    df = pull_data()
-    upload_data(df)
 
 
 if __name__ == "__main__":
     set_logging()
-    mainscript()
+    run()
