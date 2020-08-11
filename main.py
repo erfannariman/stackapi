2e6d5ac2b7c64ad4d78eb1b288bd6fd2a06e703a
Melvin Folkers <melvin@yellowstacks.nl>
2020-08-11 20:22:48 +0200

    moved some of the 'ugly' code to either the db.py or the api.py file.

--- main.py
+++ main.py
@@ -1,36 +1,22 @@
-import logging
+from src.api import pull_data
 
-from src.api import get_data
 # from src.parse_settings import get_settings
-from src.db import auth_azure
+from src.db import insert_data, append_data
+
 # from src.functions import create_dataframe
-from src.functions import MakeDataFrame
 from src.log import set_logging
 
 
-def pull_data():
-    logging.info('pulling data from Stack API...')
+def mainscript(refresh=True, upload=False):
 
-    json = get_data()
-    mkdf = MakeDataFrame(json)
-    df = mkdf.create_dataframe()
-    logging.info(f'imported {len(df)} records!')
+    if refresh:
+        df = pull_data()
 
-    return df
-
-
-def upload_data(df):
-    logging.info(f'uploading {len(df)} records to Azure...')
-    engn = auth_azure()
-    df.to_sql(name='pandas', con=engn, if_exists='replace', schema='method_usage', index=False)
-    logging.info('finished uploading!')
+    if upload:
+        # insert_data(df)
+        append_data(df)
 
 
-def mainscript():
-    df = pull_data()
-    upload_data(df)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     set_logging()
-    mainscript()
+    mainscript(refresh=True, upload=True)
