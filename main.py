19567679591624f842ac4e1cf1c50f9a1ae2fa67
Erfan Nariman | Veneficus <erfan.nariman@briccanalytics.nl>
2020-08-13 02:10:05 +0200

    FIX: moved settings out of main

--- main.py
+++ main.py
@@ -3,23 +3,21 @@ from src.api import pull_data
 from src.parse_settings import get_settings
 from src.log import set_logging
 from src.db import auth_azure, export_data
-
+from src.scraper import run_scraper
 
 settings = get_settings("settings.yml")
-REFRESH = settings["refresh"]
-UPLOAD = settings["upload"]
-METHOD = settings["method"]
-SCHEMA = settings["schema"]
 
 
 def run():
-    dfs = pull_data()
-    if UPLOAD:
+    if settings["get_pandas_methods"]:
+        run_scraper()
+        logging.info("finished scraping pandas methods")
+
+    if settings["run_api"]:
+        dfs = pull_data()
         for name, df in dfs.items():
-            export_data(
-                df=df, name=name, db_engine=auth_azure(), method=METHOD, schema=SCHEMA
-            )
-    logging.info("finished uploading!")
+            export_data(df=df, name=name, db_engine=auth_azure())
+        logging.info("finished uploading!")
 
 
 if __name__ == "__main__":
