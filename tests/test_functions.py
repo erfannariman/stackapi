54323c32e59009a5a224a67cbfce0375f4f40137
Erfan Nariman | Veneficus <erfan.nariman@briccanalytics.nl>
2020-08-15 14:54:28 +0200

    Changes for black and flake8

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -8,21 +8,25 @@ make_df = MakeDataFrame
 
 
 def test_select_string_columns():
-    df = pd.DataFrame({
-        'A': list('abc'),
-        'B': [1, 2, 3],
-        'C': pd.date_range('2020-01-01', '2020-01-03', freq='D'),
-        'D': pd.Categorical([10, 20, 30]),
-        'owner': list('def'),
-    })
+    df = pd.DataFrame(
+        {
+            "A": list("abc"),
+            "B": [1, 2, 3],
+            "C": pd.date_range("2020-01-01", "2020-01-03", freq="D"),
+            "D": pd.Categorical([10, 20, 30]),
+            "owner": list("def"),
+        }
+    )
 
-    expected = ['A']
+    expected = ["A"]
     result = make_df.select_string_columns(df)
     assert expected == result
 
 
 def test_get_user_id():
-    df = pd.DataFrame({'owner': [{'user_id': 123}, {'user_id': 456}, {'user_id': 789}]})
+    df = pd.DataFrame({
+        "owner": [{"user_id": 123}, {"user_id": 456}, {"user_id": 789}]
+    })
     expected = pd.DataFrame({"owner": [123, 456, 789]})
     result = make_df.get_user_id(df, "owner")
     tm.assert_frame_equal(expected, result)
