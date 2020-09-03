1c045645ef4b6f274216c44cf7e3c64033829715
ErfanNarimanVeneficu <erfan.nariman@briccanalytics.nl>
2020-09-04 01:23:14 +0200

    Write data to repo

--- src/git.py
+++ src/git.py
@@ -2,26 +2,17 @@ from git import Repo
 from git.exc import GitCommandError
 import logging
 
-PATH_OF_GIT_REPO = r".git"  # make sure .git folder is properly configured
 COMMIT_MESSAGE = "Write data to repo"
 
 
 def git_push():
-    logging.info("Committing export of data to repo")
-    repo = Repo(PATH_OF_GIT_REPO)
-    repo.git.add(update=True)
-    repo.index.commit(COMMIT_MESSAGE)
-    origin = repo.remote(name="origin")
-    origin.push()
-
-
-# def git_push():
-#     try:
-#         logging.info("Committing export of data to repo")
-#         repo = Repo(PATH_OF_GIT_REPO)
-#         repo.git.add(update=True)
-#         repo.index.commit(COMMIT_MESSAGE)
-#         origin = repo.remote(name='origin')
-#         origin.push()
-#     except GitCommandError:
-#         print('An error occurred while pushing the code')
+    try:
+        logging.info("Committing export of data to repo")
+        repo = Repo(".")
+        repo.git.add(update=True)
+        repo.index.commit(COMMIT_MESSAGE)
+        origin = repo.remote(name='origin')
+        current_branch = repo.active_branch.name
+        origin.push(refspec=f"master:{current_branch}")
+    except GitCommandError:
+        print('An error occurred while pushing the code')
